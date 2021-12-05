f15f9d14da2080219e910fc1ac4c0c845e498884
chimobi-justice <chimobi.justice@gmail.com>
2021-12-05 23:03:42 +0100

    fix-layout

---
+++ resources/views/components/field.blade.php
@@ -0,0 +1,3 @@
+<div class="pt-3">
+    {{ $slot }}
+</div>
---
+++ resources/views/components/form/button.blade.php
@@ -0,0 +1,3 @@
+<button type="submit" class="flex bg-red-500 uppercase font-semibold text-xs text-white py-3 px-5 rounded-2xl hover:bg-red-600">
+    {{ $slot }}
+</button>
