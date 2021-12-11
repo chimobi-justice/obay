446e862c786bbb06c46bee7de0dce1059f3feefb
chimobi-justice <chimobi.justice@gmail.com>
2021-12-11 15:42:28 +0100

    fix-login-page

--- app/Http/Controllers/sessionController.php
+++ app/Http/Controllers/sessionController.php
@@ -14,24 +14,24 @@ class sessionController extends Controller
         return view('sessions.create');
     }
     
-    // public function store(SessionUsersRequest $request)
-    // {
-    //     if (!auth()->attempt($request->only('email', 'password'), $request->remember)) {
-    //         throw ValidationException::withMessages([
-    //             'password' => 'Your provided credentials could not be verified.'
-    //         ]);
-    //     }
+    public function store(SessionUsersRequest $request)
+    {
+        if (!auth()->attempt($request->only('email', 'password'), $request->remember)) {
+            throw ValidationException::withMessages([
+                'password' => 'Your provided credentials could not be verified.'
+            ]);
+        }
 
-    //     session()->regenerate();
+        session()->regenerate();
 
-    //     return redirect()->route(RouteServiceProvider::DASHBOARD)->with('status', 'Welcome back');
+        return redirect()->route(RouteServiceProvider::DASHBOARD)->with('status', 'Welcome back');
 
-    // }
+    }
 
-    // public function destroy()
-    // {
-    //     auth()->logout();
+    public function destroy()
+    {
+        auth()->logout();
 
-    //     return redirect(RouteServiceProvider::LOGIN);
-    // }
+        return redirect(RouteServiceProvider::LOGIN);
+    }
 }
